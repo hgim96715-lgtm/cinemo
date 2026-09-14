757d7da09ead7f8a7bbfd6cd0f9a1727e870319c
hgim96715-lgtm <hgim96715@gmail.com>
2026-09-14 09:16:30 +0900

    api-fetch import 경로 수정

--- apps/web/lib/lobby-board-api.ts
+++ apps/web/lib/lobby-board-api.ts
@@ -1,5 +1,5 @@
 import type { LobbyBoardResponse } from '@cinemo/shared';
-import { apiFetch } from './api';
+import { apiFetch } from './api-fetch';
 
 export function getLobbyBoardRequest() {
   return apiFetch<LobbyBoardResponse>('/lobby/board');
